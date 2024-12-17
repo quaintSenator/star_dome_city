3f7af0a8ca82942524efc559ad1f306bc9aae66a
quaintSenator <244450353@qq.com>
2024-12-18 01:44:55 +0800

    [SDC-1] #1 创建工程

---
+++ Assets/Scripts/BaseFactory.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct FactoryConfig
+{
+    public int inputPipeNum;
+    public int outputPipeNum;
+}
+public class BaseFactory : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
