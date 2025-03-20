ebbc252914add4876a5a6c48be1365b4e434a3ee
quaintSenator <244450353@qq.com>
2025-03-21 02:01:52 +0800

    [PCP-24-程序] 自动跑表程序 #1 matter table

---
+++ Assets/Scripts/Tables/MatterTable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Config
+{
+    public class SingletonBase<T> where T : new()
+    {
+        private static T instance;
+        protected SingletonBase() { } // 保护构造函数，防止外部直接实例化
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new T();
+                }
+                return instance;
+            }
+        }
+    }
+    public class MatterTable : SingletonBase<MatterTable>
+    {
+        #region auto_generate_start
+        //
+        List<int> numbers = new List<int>(new int[] { 1, 2, 3, 4, 5 });
+
+        #endregion auto_generate_end
+    }
+}
---
+++ Assets/Scripts/Tables/ReactionTable.cs
@@ -0,0 +1,7 @@
+﻿namespace Config
+{
+    public class ReactionTable : SingletonBase<MatterTable>
+    {
+        
+    }
+}
